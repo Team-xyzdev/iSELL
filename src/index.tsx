51d89c06baca408175a5f23b152da4fd250c69f6
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-08-30 13:04:17 +0100

    MVC + using redux store 🏷

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
+// Copyright Paylancers 💳 2022 
+// 17 U.S.C §§ 101-1511
+
+// relevant modules and files
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
+// styles
+import './index.css';
 
+// building block
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
