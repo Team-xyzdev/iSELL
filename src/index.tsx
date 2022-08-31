9ba7fbe46a1c1c7ea2727884db5267b151e13de3
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-08-31 10:58:53 +0100

    setting redux store + cleaning ⚙️

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import store from './store/store';
+import {store} from './store/store';
 import { Provider } from 'react-redux';
 
 // styles
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/user.reducer';
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
       currentUser : userReducer
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
