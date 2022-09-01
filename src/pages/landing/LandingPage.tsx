e01e34a8e0e4c5cc7f16de696943c400b53e4b2b
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-09-01 12:33:53 +0100

    MVC - file structuring and readability 📲

---
+++ src/pages/landing/LandingPage.tsx
@@ -0,0 +1,23 @@
+// Copyright iSELL 💳 2022 
+// 17 U.S.C §§ 101-1511
+
+//importing relevant modules and files
+import * as  React from "react";
+import { Link } from "react-router-dom";
+
+// JSX  component
+const LandingPage = () => {
+  return (
+    <div>
+      
+      {/* <Link to="/register">
+        <p>Register</p>
+      </Link>
+      <Link to="/login">
+        <p>Login</p>
+      </Link> */}
+    </div>
+  )
+};
+
+export default LandingPage;
---
+++ src/pages/register/RegisterPage.tsx
@@ -0,0 +1,15 @@
+// Copyright iSELL 💳 2022 
+// 17 U.S.C §§ 101-1511
+import React from "react";
+import Register from "../../components/register/register.component";
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <Register />
+        </div>
+    )
+}
+
+export default RegisterPage;
+
