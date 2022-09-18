c213a630828c843a34249052d2e74e092db5cef7
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-09-18 13:39:03 +0100

    adding conditional dashboard product page + spinner 🥑

--- client/src/components/dashboard/dashboard-products/dashboard-products.component.tsx
+++ client/src/components/dashboard/dashboard-products/dashboard-products.component.tsx
@@ -8,8 +8,10 @@ import {UilPlus} from '@iconscout/react-unicons';
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
 import { db, getProducts } from "../../../firebase/firebase.utils";
+import memoize from 'lodash.memoize';
 import { doc, getDoc } from "firebase/firestore";
 import Spinner from "../../spinner/spinner";
+import { Link } from "react-router-dom";
 
 //import logo
 const uploadImg = require('../../../assets/upload.png');
@@ -19,10 +21,11 @@ const DashboardProducts = () => {
   const getUserUid: any = useSelector(
     (state: RootState) => state.currentUser.currentUser
   );
-  const [products, setProducts] = useState([]);
+  const [products, setProducts]:any = useState([]);
   const [loading, setloading] = useState(false);
+  const [name, setName] = useState("")
 
-  const getVendorProducts = async () => {
+  const getVendorProducts = memoize(async () => {
     setloading(false);
     if(!getUserUid) return
     const getDocRef = doc(db, "users", getUserUid);
@@ -30,10 +33,11 @@ const DashboardProducts = () => {
     const userSnapshot=  await getDoc(getDocRef);
     if (userSnapshot?.data()?.verification) {
       setProducts(userSnapshot.data()?.products)
+      setName(userSnapshot.data()?.businessDetails?.business_name)
       setloading(true);
     }
   
-  }
+  })
 
   
   useEffect(() => {
@@ -43,26 +47,57 @@ const DashboardProducts = () => {
     return(
      <div className="dashboard__upload">
        {
-         !loading && 
-         <Spinner />
+         !loading ? 
+         <Spinner />  
+         :
+         products.length> 0 && loading ? 
+
+         <div className="vendor__products">
+           <div className="add__products">
+            <div className="products__main">
+             <h2> Products</h2> 
+             <p>Store owned by {name}</p>
+            </div>
+            <Link to='/dashboard/create'>
+            <button>Add Item</button>
+            </Link>
+            
+           </div>
+          
+           <div className="products">
+           
+              {
+                products.map((product, i) =>
+                (
+                 <div key={i}>
+                  <div className="product">
+                   <p>{product?.stock}</p>
+                    <img src={product?.imageUrl} />
+                  </div>
+                 <div className="product__details">
+                   <h2>{product?.product}</h2>
+                   <p>{product?.price}</p>
+                 </div>
+               </div>
+                ))
+              }
+              </div>
+          
+          </div>
+         
+         : 
+         <div className="dashboard-products">
+         <img src={uploadImg} alt="upload icon" />
+         <h2> Start by uploading a product</h2>
+         <p> Any product you upload on iSELL will live here.</p>
+         <p> Start creating your Products.</p>
+         <button className="product__button">
+                 <UilPlus className="uil__plus"/>
+                 <span> Create A Product</span>
+          </button>
+       </div> 
        }
-      {
-        products && loading &&  
-        <p> products</p>
-      }
-      {
-        !products && !loading &&
-       <div className="dashboard-products">
-        <img src={uploadImg} alt="upload icon" />
-        <h2> Start by uploading a product</h2>
-        <p> Any product you upload on iSELL will live here.</p>
-        <p> Start creating your Products.</p>
-        <button className="product__button">
-                <UilPlus className="uil__plus"/>
-                <span> Create A Product</span>
-         </button>
-      </div> 
-      }
+  
   
 
      </div>
